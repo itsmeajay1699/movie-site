d041bbd71b69d8c22f23699dfbc37e6fbb382656
itsmeajay1699 <112202679+itsmeajay1699@users.noreply.github.com>
2023-04-25 17:09:24 +0530

    custom hook for data fetching

---
+++ src/api/useFetch.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const useFetch = (url) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [apiData, setApiData] = useState(null);
+  const [serverError, setServerError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const resp = await axios.get(
+          `${import.meta.env.VITE_REACT_APP_TMBD_API_BASE_URL}${url}?api_key=${
+            import.meta.env.VITE_REACT_APP_TMBD_API_KEY
+          }`
+        );
+        
+        setApiData(resp.data.results);
+        setIsLoading(false);
+      } catch (error) {
+        setServerError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { isLoading, apiData, serverError };
+};
+
+export default useFetch;
